036760f1f4efc074b695ddbd8d9188dcf70421b8
otie16 <biggieestone@gmail.com>
2024-09-30 15:02:53 +0100

    call lib function in jenkins file

--- script.groovy
+++ script.groovy
@@ -1,18 +1,3 @@
-def buildJar(){
-    echo "building the application..."
-    sh 'mvn package'
-}
-
-def buildImage(){
-    echo "building the docker image..."
-    // Extracting the github credentials from jenkins 
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-repo-credentials', passwordVariable: 'PASS', usernameVariable: 'USER', )]){
-        sh 'docker build -t otobongedoho18361/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push otobongedoho18361/demo-app:jma-2.0'
-    } 
-}
-
 def deployApp(){
     echo "deploying the application..."
 }
